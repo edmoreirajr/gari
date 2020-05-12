5daf036492e0baef6f1c4e1166b5bf39055a4326
Edvaldo Júnior <edvaldojunior@outlook.com>
2020-05-12 12:59:36 -0300

    Limpeza e finalização.
    - Adicionado blip principal dos garis no mapa;
    - Limpeza da array de localização;

--- server.lua
+++ server.lua
@@ -5,7 +5,7 @@ RegisterServerEvent('esx_garijob:onNPCJobMissionCompleted')
 AddEventHandler('esx_garijob:onNPCJobMissionCompleted', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	local total   = math.random(faxina_min, faxina_max);
+	local total   = math.random(Config.faxina_min,Config.faxina_max);
 	xPlayer.addMoney(total)
 	TriggerClientEvent('esx:showNotification', source, '~y~Limpeza concluída, Você recebeu ~p~'..total..' ~y~reais.')
 	TriggerClientEvent('esx:showNotification', source, '~y~Vá para o próximo local de limpeza ou pressione ~p~F7 ~y~para sair do expediente.')
